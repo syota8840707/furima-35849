d7e7a341ab3276570841c9cafcd088589bb1dd4f
syota8840707 <za.mazyan8840707@gmail.com>
2021-06-10 16:59:01 +0900

    ユーザー管理機能・単体テスト実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   
-  validates :nickname,  presence: true
-  validates :last_name,  presence: true
-  validates :first_name,  presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana,  presence: true
-  validates :birthday,  presence: true
+
+  validates :nickname, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角ひらがな、全角カタカナ、漢字で入力して下さい' }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
+  validates :birthday, presence: true
+
+  validates :password, :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   has_many :items
 end
