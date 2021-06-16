f80563275c1deb6ab3a6b84c8d054760b527e977
syota8840707 <za.mazyan8840707@gmail.com>
2021-06-16 13:58:55 +0900

    修正依頼改善２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -23,10 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
-    unless  current_user.id == @item.user_id
-    redirect_to action: :index 
-    end
+
   end
 
   def update
@@ -47,4 +45,9 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end  
+  def move_to_index
+    unless  current_user.id == @item.user_id
+      redirect_to action: :index 
+      end
+    end    
 end
